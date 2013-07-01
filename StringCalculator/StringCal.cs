bb35e657c2d915d592a2cd57cde7d0e7038cb379
Arne Noehre <arne.noehre@dff-solutions.de>
2013-07-01 20:47:30 +0200

    Initial commit
    My solution of string calculator kata

---
+++ StringCalculator/StringCal.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using dff.Extensions;
+
+namespace StringCalculator
+{
+    public class StringCal
+    {
+        public int Add(string lineOfText)
+        {
+            if (string.IsNullOrEmpty(lineOfText)) return 0;
+
+            var singleCharSeperators = new List<char> {',', '\n'};
+            
+            var customSeperator = StringSpereratorGet(lineOfText);
+            if (customSeperator!=null && customSeperator.Any()) 
+                lineOfText = RemoveStringSeperatorLine(lineOfText);
+
+            var extendedList = new List<string>();
+            var listSplittedOnDefaultSeperators = lineOfText.Split(singleCharSeperators.ToArray()).ToList();
+
+            if (customSeperator != null && customSeperator.Any())
+            {
+                foreach (var item in listSplittedOnDefaultSeperators)
+                {
+                    var stringSeparators = customSeperator.ToArray();
+                    extendedList.AddRange(item.Split(stringSeparators, StringSplitOptions.RemoveEmptyEntries).ToList());
+                }
+            }
+            else extendedList = listSplittedOnDefaultSeperators;
+
+            return extendedList.Where(x=>x.TryToInt()<1000).Sum(s => s.TryToInt());
+        }
+
+        private static string RemoveStringSeperatorLine(string line)
+        {
+            line = line.Substring(line.IndexOf("\n", System.StringComparison.Ordinal) + 1);
+            return line;
+        }
+
+        private static List<string> StringSpereratorGet(string line)
+        {
+            var seperators= new List<string>();
+            if (line.StartsWith("//"))
+            {
+                var x = line.Substring(2, line.IndexOf("\n", System.StringComparison.Ordinal) - 2);
+                seperators = x.Split(',').ToList();
+            }
+            return seperators;
+        }
+    }
+}
---
+++ StringCalculator/StringCalculatorTests.cs
@@ -0,0 +1,92 @@
+﻿using NUnit.Framework;
+
+namespace StringCalculator
+{
+    public class StringCalculatorTests
+    {
+        [Test]
+        public void AddZero()
+        {
+            var sc = new StringCal();
+            var sum=sc.Add("");
+
+            Assert.AreEqual(0, sum);
+        }
+        
+        [Test]
+        public void AddOne()
+        {
+            var sc = new StringCal();
+            var sum=sc.Add("1");
+
+            Assert.AreEqual(1, sum);
+        }
+        
+        [Test]
+        public void AddTwoDigits()
+        {
+            var sc = new StringCal();
+            var sum=sc.Add("1,2");
+
+            Assert.AreEqual(3, sum);
+        }
+        
+        [Test]
+        public void AddFourDigits()
+        {
+            var sc = new StringCal();
+            var sum=sc.Add("1,2,3,5");
+
+            Assert.AreEqual(11, sum);
+        }
+        
+        [Test]
+        public void AddFourDigitsWithLineBreak()
+        {
+            var sc = new StringCal();
+            var sum=sc.Add("1,2,3\n5");
+
+            Assert.AreEqual(11, sum);
+        }
+        
+        [Test]
+        public void AddFourDigitsWithLineBreakFirstLineIsTheSpeperator()
+        {
+            var sc = new StringCal();
+            var sum=sc.Add("//X\n1X2X3X5");
+
+            Assert.AreEqual(11, sum);
+        }
+
+        [Test]
+        public void AddFourDigitsWithLineBreakFirstLineIsTheSpeperatorWithMultipleChars()
+        {
+            var sc = new StringCal();
+            var sum=sc.Add("//XXX\n1XXX2XXX3XXX5");
+
+            Assert.AreEqual(11, sum);
+        }
+        
+        [Test]
+        public void AddFourDigitsWithLineBreakFirstLineIsTheSpeperatorWithMultipleCharsWithMultipleSeperators()
+        {
+            var sc = new StringCal();
+            var sum=sc.Add("//XXX,yyy\n1XXX2yyy3XXX5");
+
+            Assert.AreEqual(11, sum);
+        }
+        
+        [Test]
+        public void IgnoreNumbersBiggerThanThousand()
+        {
+            var sc = new StringCal();
+            var sum=sc.Add("1,2,1000");
+
+            Assert.AreEqual(3, sum);
+        }
+
+
+
+
+    }
+}
