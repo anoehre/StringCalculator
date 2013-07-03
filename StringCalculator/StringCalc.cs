38f1f4bf20c2e72ed935517af212e3ae5cf40e28
Arne Noehre <arne.noehre@dff-solutions.de>
2013-07-03 16:17:08 +0200

    Fertig

---
+++ StringCalculator/StringCalc.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using dff.Extensions;
+
+namespace StringCalculator
+{
+    public class StringCalc
+    {
+        public int Add(string line)
+        {
+            if (string.IsNullOrEmpty(line)) return 0;
+            var result = 0;
+
+            var separator = new List<string> { ",", "\n" };
+            if (line.StartsWith("//"))
+            {
+                var speratorLine=line.Substring(2, line.IndexOf("\n", StringComparison.Ordinal)-2);
+                separator.AddRange(speratorLine.Split(",".ToCharArray(), StringSplitOptions.RemoveEmptyEntries).ToList());
+                line = line.Substring(line.IndexOf("\n", StringComparison.Ordinal) + 1);
+            }
+            
+            var items = line.Split(separator.ToArray(), StringSplitOptions.RemoveEmptyEntries);
+
+            var errorItems = new List<string>();
+            foreach (var item in items)
+            {
+                int parsedInteger = item.TryToInt();
+                if (parsedInteger<0) errorItems.Add(item);
+                if (parsedInteger > 1000) parsedInteger = 0;
+                result += parsedInteger;    
+            }
+
+            if (errorItems.Any())
+                throw new Exception("Negative Value detected!: " + errorItems.Aggregate( (s,e) => s+","+e));
+
+            return result;
+        }
+    }
+}
---
+++ StringCalculator/StringClaculatorTests.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace StringCalculator
+{
+    public class StringClaculatorTests
+    {
+        [Test]
+        public void AddNull()
+        {
+            var calc = new StringCalc();
+            var result = calc.Add(null);
+            Assert.AreEqual(0, result);
+        }
+        
+        [Test]
+        public void AddOneNumber()
+        {
+            var calc = new StringCalc();
+            var result = calc.Add("5");
+            Assert.AreEqual(5, result);
+        }
+        
+        [Test]
+        public void AddTwoNumbers()
+        {
+            var calc = new StringCalc();
+            var result = calc.Add("5,4");
+            Assert.AreEqual(9, result);
+        }
+        
+        [Test]
+        public void AddThreeNumbers()
+        {
+            var calc = new StringCalc();
+            var result = calc.Add("5,4,1");
+            Assert.AreEqual(10, result);
+        }
+        
+        [Test]
+        public void AddThreeNumbersWithLineBreak()
+        {
+            var calc = new StringCalc();
+            var result = calc.Add("5,4\n1");
+            Assert.AreEqual(10, result);
+        }
+        
+        [Test]
+        public void AddThreeNumbersWithCustomSeperator()
+        {
+            var calc = new StringCalc();
+            var result = calc.Add("//+\n5,4+1");
+            Assert.AreEqual(10, result);
+        }
+        
+        [Test]
+        public void AlertWhenNegativeNumbersIncluded()
+        {
+            var calc = new StringCalc();
+            var ex=Assert.Throws<Exception>(() => calc.Add("//+\n5,-4+-1"));
+            Console.WriteLine(ex.Message);
+        }
+
+        [Test]
+        public void CatchInvalidIntegers()
+        {
+            var calc = new StringCalc();
+            var result = calc.Add("//+\n5.22,4+1");
+            Assert.AreEqual(5, result);
+        }
+        
+        [Test]
+        public void IgnoreValuesBiggerThanTousand()
+        {
+            var calc = new StringCalc();
+            var result = calc.Add("//+\n1001,4+1");
+            Assert.AreEqual(5, result);
+        }
+        
+        [Test]
+        public void SupportMultiCharDelimiter()
+        {
+            var calc = new StringCalc();
+            var result = calc.Add("//-+-\n5-+-4-+-1");
+            Assert.AreEqual(10, result);
+        }
+        
+        [Test]
+        public void SupportMultipleMultiCharDelimiter()
+        {
+            var calc = new StringCalc();
+            var result = calc.Add("//-+-,(())\n5-+-4-+-1(())11");
+            Assert.AreEqual(21, result);
+        }
+    }
+}
