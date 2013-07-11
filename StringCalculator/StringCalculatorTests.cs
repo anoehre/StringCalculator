cf8ec1c0b0e8be469cd310ee6171998241bd17dc
Arne Noehre <arne.noehre@dff-solutions.de>
2013-07-11 17:01:20 +0200

    Fertig

---
+++ StringCalculator/StringCalculatorTests.cs
@@ -0,0 +1,92 @@
+﻿#region
+
+using System;
+using NUnit.Framework;
+
+#endregion
+
+namespace StringCalculator
+{
+    public class StringCalculatorTests
+    {
+        [Test]
+        public void AddNull()
+        {
+            var c = new StringClac();
+            var x = c.Add(null);
+            Assert.AreEqual(0, x);
+        }
+
+        [Test]
+        public void AddOneNumber()
+        {
+            var c = new StringClac();
+            var x = c.Add("1");
+            Assert.AreEqual(1, x);
+        }
+
+        [Test]
+        public void AddTwoNumbers()
+        {
+            var c = new StringClac();
+            var x = c.Add("1,3");
+            Assert.AreEqual(4, x);
+        }
+
+        [Test]
+        public void AddTThreeNumbers()
+        {
+            var c = new StringClac();
+            var x = c.Add("1,3,10");
+            Assert.AreEqual(14, x);
+        }
+
+        [Test]
+        public void AddThreeNumbersWithLinebreak()
+        {
+            var c = new StringClac();
+            var x = c.Add("1,3\n10");
+            Assert.AreEqual(14, x);
+        }
+
+        [Test]
+        public void AddThreeNumbersWithCustomDelimiter()
+        {
+            var c = new StringClac();
+            var x = c.Add("//;\n1,3;10");
+            Assert.AreEqual(14, x);
+        }
+
+        [Test]
+        public void ExceptionThrownWhenNegative()
+        {
+            var c = new StringClac();
+            var x = Assert.Throws<Exception>(() => c.Add("//;\n1,-3;10"));
+            Console.WriteLine(x.Message);
+        }
+
+        [Test]
+        public void NumbersGreater1000Ignored()
+        {
+            var c = new StringClac();
+            var x = c.Add("//;\n1,1001;10");
+            Assert.AreEqual(11, x);
+        }
+
+        [Test]
+        public void DelimiterWithAnyFormat()
+        {
+            var c = new StringClac();
+            var x = c.Add("//ABC\n1,1001ABC10");
+            Assert.AreEqual(11, x);
+        }
+
+        [Test]
+        public void MultipleDelimiterWithAnyFormat()
+        {
+            var c = new StringClac();
+            var x = c.Add("//ABC,CBA\n1CBA1001ABC10");
+            Assert.AreEqual(11, x);
+        }
+    }
+}
---
+++ StringCalculator/StringClac.cs
@@ -0,0 +1,31 @@
+﻿#region
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+#endregion
+
+namespace StringCalculator
+{
+    public class StringClac
+    {
+        public int Add(string line)
+        {
+            if (string.IsNullOrEmpty(line)) return 0;
+
+            var separator = new List<string> {",", "\n"};
+            if (line.StartsWith("//"))
+            {
+                separator.AddRange(line.Substring(2, line.IndexOf("\n", StringComparison.Ordinal) - 2).Split(','));
+                line = line.Substring(line.IndexOf("\n", StringComparison.Ordinal) + 1);
+            }
+
+            var numbers = line.Split(separator.ToArray(), StringSplitOptions.RemoveEmptyEntries);
+
+            if (numbers.Any(x => int.Parse(x) < 0)) throw new Exception("Negatives not allowed");
+
+            return numbers.Where(x => int.Parse(x) <= 1000).Sum(number => int.Parse(number));
+        }
+    }
+}
